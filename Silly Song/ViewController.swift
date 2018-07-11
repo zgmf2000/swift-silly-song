5be3d2d4f0ced5fe21539e62c9279698deb89028
Bintoro Adi Guna <bintoro.guna@btpn.com>
2018-07-11 13:58:24 +0700

    Initial Commit

---
+++ Silly Song/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Silly Song
+//
+//  Created by Bintoro Adi Guna on 7/11/18.
+//  Copyright © 2018 Vector Industries. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
